880dae414f7d4f606a302914a43d80281dbc0779
Burak Çalık <burak@macellan.net>
2015-06-12 00:47:44 +0300

    Indentation fix

--- sifter.js
+++ sifter.js
@@ -42,10 +42,10 @@
 	 * To lowercase for Turkish
 	 */
 	String.prototype.turkishToLower = function(){
-    var string = this;
-    var letters = { "İ": "i", "I": "ı", "Ş": "ş", "Ğ": "ğ", "Ü": "ü", "Ö": "ö", "Ç": "ç" };
-    string = string.replace(/(([İIŞĞÜÇÖ]))/g, function(letter){ return letters[letter]; })
-    return string.toLowerCase();
+		var string = this;
+		var letters = { "İ": "i", "I": "ı", "Ş": "ş", "Ğ": "ğ", "Ü": "ü", "Ö": "ö", "Ç": "ç" };
+		string = string.replace(/(([İIŞĞÜÇÖ]))/g, function(letter){ return letters[letter]; })
+		return string.toLowerCase();
 	}
 
 	/**
